1210c7a145ab6846e130e70ffe8eace2a94b54a5
yungNATE <nathan.martinigol@gmail.com>
2023-03-22 19:23:42 +0100

    POC : zoom/dezoom WIP

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/main.js"></script>
+    
     <title>Beksiński</title>
 </head>
 <body id="index">
@@ -13,18 +16,19 @@
     </div>
 
     <div id="poc">
-    <section id="liquidEffect">
+        <!-- <section id="liquidEffect">
+            <script src="js/libs/three.js/three.min.js"></script>
+            <script src="js/libs/gsap/gsap.min.js"></script>
+            <script src="js/libs/hover-effect/dist/hover-effect.umd.js"></script>
 
-        <script src="js/libs/three.js/three.min.js"></script>
-        <script src="js/libs/gsap/gsap.min.js"></script>
-        <script src="js/libs/hover-effect/dist/hover-effect.umd.js"></script>
-        <script src="js/main.js"></script>
+            <div class="liquidEffect-wapper"></div>
+        </section> -->
 
-        <div class="poc distortion"></div>
-
-        <div class="poc zoomdezoom"></div>
-
-    </section>
+        <section id="zoomdezoom">
+            <div class="zoomdezoom-wrapper">
+                <img src="media/img/visage_ville.jpg" alt="">
+            </div>
+        </section>
     </div>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+zoomLevel = 1;
 window.onload = function() {
+    
+    //* POC : Zoom / Dézoom
+    var zoomDezoom = document.querySelector('#zoomdezoom > div > img');
+    var zoomAcceleration = 0.01
+    const ZOOMLVLMAX = 95; // valeur arbitraire qui marche
 
 
+    addEventListener('wheel', function() {
+        if(zoomLevel < ZOOMLVLMAX) { // TODO : arriver à virer l'event listener
+            this.removeEventListener('wheel') 
+        }
+        zoomAcceleration *= 1.1 // plus on zoom, plus c'est chiant et lent. Ici, plus on zoom, plus c'est rapide
+        zoomDezoom.style.transform = `scale(${zoomLevel += zoomAcceleration})`;
+    });
+
+}
+
+
+function distortionPOC(){
     //* POC : Distortion
     isTriggeredByHover = true; // sinon trigger par clic
-    var distortion = document.querySelector('.distortion');
+    var distortion = document.querySelector('.liquidEffect-wrapper');
     
     
     var hoverAnimation = new hoverEffect({
@@ -26,15 +44,4 @@ window.onload = function() {
             
         });
     }
-
-
-    //* POC : Zoom / Dézoom
-    var zoomDezoom = document.querySelector('.zoomdezoom');
-    var zoomLevel = 1;
-
-    // on scroll event, i want zoomdezoom var to zoom
-    addEventListener('scroll', function() {
-        zoomDezoom.style.transform = `scale(${zoomLevel += 0.01})`;
-    });
-
 }
