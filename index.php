f3a7693905cfe1c97f5bf6648f23307a1d1d83a6
yungNATE <nathan.martinigol@gmail.com>
2023-03-22 11:59:08 +0100

    POC : liquid image transition

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Beksiński</title>
+</head>
+<body id="index">
+    <div id="canva">
+
+    </div>
+
+    <div id="poc">
+    <section id="liquidEffect">
+
+        <script src="js/libs/three.js/three.min.js"></script>
+        <script src="js/libs/gsap/gsap.min.js"></script>
+        <script src="js/libs/hover-effect/dist/hover-effect.umd.js"></script>
+        <script src="js/main.js"></script>
+
+        <div class="distortion"></div>
+
+    </section>
+    </div>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+    new hoverEffect({
+        parent: document.querySelector('.distortion'),
+        intensity: 0.2,
+        image1: '../beksinski/media/img/hug.jpg',
+        image2: '../beksinski/media/img/hug-wall.jpg',
+        displacementImage: 'js/libs/hover-effect/images/heightMap.png',
+        
+    })
+
+}
