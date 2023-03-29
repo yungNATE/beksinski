d347c2b9fde17bfe95f4e634eb7c22e1d01510da
yungNATE <nathan.martinigol@gmail.com>
2023-03-29 11:21:44 +0200

    liquid effect 2 WIP

---
+++ js/poc/liquidEffect2.js
@@ -0,0 +1,80 @@
+window.addEventListener("resize", onWindowResize);
+var frustumSize = 600;
+var aspect = window.innerWidth / window.innerHeight;
+var clock = new THREE.Clock(true);
+var scene = new THREE.Scene();
+camera = new THREE.OrthographicCamera(
+	(frustumSize * aspect) / -2,
+	(frustumSize * aspect) / 2,
+	frustumSize / 2,
+	frustumSize / -2,
+	1,
+	1000
+);
+camera.lookAt(scene.position);
+camera.position.z = 1;
+var renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio = 1;
+renderer.setClearColor(0xffffff, 1.0);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+var parent = document.body;
+function onWindowResize() {
+	var aspect = window.innerWidth / window.innerHeight;
+	camera.left = (-frustumSize * aspect) / 2;
+	camera.right = (frustumSize * aspect) / 2;
+	camera.top = frustumSize / 2;
+	camera.bottom = -frustumSize / 2;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+var addToGPU = function (t) {
+	renderer.setTexture2D(t, 0);
+};
+var loader = new THREE.TextureLoader();
+loader.crossOrigin = "";
+var texture1 = loader.load(
+	"https://robindelaporte.fr/codepen/loader/0.png",
+	function (texture) {
+		addToGPU(texture);
+	}
+);
+var texture2 = loader.load(
+	"https://robindelaporte.fr/codepen/loader/10.png",
+	function (texture) {
+		addToGPU(texture);
+	}
+);
+var disp = loader.load("https://robindelaporte.fr/codepen/loader/fluid.jpg");
+disp.wrapS = disp.wrapT = THREE.RepeatWrapping;
+var mat = new THREE.ShaderMaterial({
+	uniforms: {
+		time: { type: "f", value: 1.0 },
+		angle1: { type: "f", value: Math.PI / 4 },
+		angle2: { type: "f", value: -(Math.PI / 4) * 3.0 },
+		random: { type: "f", value: 0.0 },
+		dispFactor: { type: "f", value: 0.0 },
+		texture: { type: "t", value: texture1 },
+		texture2: { type: "t", value: texture2 },
+		disp: { type: "t", value: disp },
+		res: {
+			type: "vec4",
+			value: new THREE.Vector4(parent.offsetWidth, parent.offsetHeight, 1.0, 1.0)
+		}
+	},
+	vertexShader: document.getElementById("vertexShader").textContent,
+	fragmentShader: document.getElementById("fragmentShader").textContent,
+	transparent: true,
+	opacity: 1.0
+});
+var geometry = new THREE.PlaneBufferGeometry(600, 600, 1);
+var object = new THREE.Mesh(geometry, mat);
+scene.add(object);
+var animate = function () {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+	var d = clock.getDelta();
+	mat.uniforms.time.value += d;
+};
+animate(); 
+this.GUI = new dat.GUI(); this.GUI.add(mat.uniforms.dispFactor, "value", 0, 1, 0.01).name("transition");
