77da42fa60d7f8f1f1bfa055bd883875eb2d9a3b
smanchajm <samuel.manchajm@gmail.com>
2023-05-07 16:39:56 +0200

    test cursor

--- js/cursor/cursor.js
+++ js/cursor/cursor.js
@@ -1,13 +1,30 @@
+let borderWidth = 2;
+let scale = 1;
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    cursor.setAttribute("style", "top: "+(e.pageY - 20)+"px; left: "+(e.pageX - 20)+"px;")
+     borderWidth = 2;
 })
 
+
 document.addEventListener('click', () => {
-    cursor.classList.add("expand");
+    /*cursor.classList.add("expand");
 
     setTimeout(() => {
         cursor.classList.remove("expand");
     }, 500)
+
+    cursor.classList.add("clicked");
+
+    setTimeout(() => {
+        cursor.classList.remove("clicked");
+    }, 1000)
+
+    borderWidth *= 2;
+    cursor.style.borderWidth = `${borderWidth}px`;*/
+    scale *= 1.5; // Augmenter la mise à l'échelle de 50% à chaque clic
+    box.style.transform = `scale(${scale})`;
+
+    
 })
