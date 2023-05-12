f17e52051cbb30dd6262ecaa8aa41e223bf4073a
yungNATE <nathan.martinigol@gmail.com>
2023-05-12 14:50:29 +0200

    test img 360

--- js/poc/360.js
+++ js/poc/360.js
@@ -1,6 +1,6 @@
 const threeSixtyMain = () => {
 
-    const panorama = new PANOLENS.ImagePanorama( '../../beksinski/media/img/field.jpg' );
+    const panorama = new PANOLENS.ImagePanorama( '../../beksinski/media/img/fieldTest.jpg' );
     const viewer = new PANOLENS.Viewer();
     viewer.add( panorama );
 
