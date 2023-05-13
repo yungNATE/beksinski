b9fdf5e1894d37de9f64453e16d05e1ca403d169
yungNATE <nathan.martinigol@gmail.com>
2023-05-13 14:52:14 +0200

    intro WIP

--- js/poc/360.js
+++ js/poc/360.js
@@ -1,6 +1,7 @@
 const threeSixtyMain = () => {
 
-    const panorama = new PANOLENS.ImageLittlePlanet( '../../beksinski/media/img/fieldTest.jpg' );
+    // const panorama = new PANOLENS.ImageLittlePlanet( '../../beksinski/media/img/fieldTest.jpg' );
+    const panorama = new PANOLENS.ImagePanorama( '../../beksinski/media/img/fieldTest.jpg' );
     const viewer = new PANOLENS.Viewer();
     viewer.add( panorama );
 
