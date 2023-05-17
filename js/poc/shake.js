eaa57c0bd27f6a7e3619faae661b66676062a413
smanchajm <samuel.manchajm@gmail.com>
2023-05-17 15:50:39 +0200

    shake + sound

--- js/poc/shake.js
+++ js/poc/shake.js
@@ -1,9 +1,15 @@
 function startShaking() {
+    
+    var explosion = new Howl({
+        src: ['../music/sf_explosion_20.mp3'],
+    })
+    explosion.play();
+    
     var image = document.getElementById("shake_image");
     image.classList.add("shaking");
     
     // Optionnel : Arrêter le tremblement après un certain délai
     setTimeout(function() {
       image.classList.remove("shaking");
-    }, 1000); // Tremblement arrêté après 2 secondes (2000 millisecondes)
+    }, 1000); 
   }
