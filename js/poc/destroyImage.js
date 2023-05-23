4e111b53073601af32e1a67856a0f21ead5577d5
yungNATE <nathan.martinigol@gmail.com>
2023-05-23 19:28:40 +0200

    Effet de destruction

--- js/poc/destroyImage.js
+++ js/poc/destroyImage.js
@@ -7,15 +7,24 @@ const destroyImageMain = () => {
 }
 addEventListener('load', destroyImageMain);
 
+let brightness = 1;
+let sizeMultiplier = 1;
 let destroy = function(e, destroyedImage){
+    if(brightness < 0.1) destroyedImage.remove();
+
     let svgWidth = destroyedImage.getBBox().width;
     let svgHeight = destroyedImage.getBBox().height;
     
-    let randomWidth = Math.floor(Math.random() * (svgWidth / 2) );
-    let randomHeight = Math.floor(Math.random() * (svgHeight / 2) );
+    sizeMultiplier *= 1.1
+    let randomWidth = Math.floor(Math.random() * ((svgWidth / 4) * (sizeMultiplier)) );
+    let randomHeight = Math.floor(Math.random() * ((svgWidth / 4) * (sizeMultiplier)) );
+    // Créer ici une fissure de taille aléatoire ⬆️
+
+
+
     let randomRotation = Math.floor(Math.random() * 360);
-    let randomX = Math.floor(Math.random() * (svgWidth - randomWidth) );
-    let randomY = Math.floor(Math.random() * (svgHeight - randomHeight) );
+    let randomX = Math.floor(Math.random() * (randomWidth) );
+    let randomY = Math.floor(Math.random() * (randomHeight) );
     
 
     let newHole = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
@@ -33,9 +42,7 @@ let destroy = function(e, destroyedImage){
     destroyedImage.querySelector("#hole").appendChild(newHole);
 
 
-    // TODO : WIP v
     // darken image each time a hole is created
-    console.log(destroyedImage.style.filter);
     let darkFilter =  destroyedImage.style.filter == 0 ? 0.1 : parseFloat(destroyedImage.style.filter.match('/\d+/')) * 1.1;
-    destroyedImage.style.filter = `brightness(${darkFilter})`;
+    destroyedImage.style.filter = `brightness(${brightness /= 1.1})`;
 }
