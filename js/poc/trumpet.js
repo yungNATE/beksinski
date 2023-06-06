dc6899560f2f227b46c0b1aef60c567b1a979a67
smanchajm <samuel.manchajm@gmail.com>
2023-06-06 10:46:45 +0200

    test musique

--- js/poc/trumpet.js
+++ js/poc/trumpet.js
@@ -16,8 +16,7 @@
 
  document.addEventListener('DOMContentLoaded', function() {
     var sound = new Howl({
-      src: ['../music/clarinetmusic.mp3'],
-      //src: ['../music/DarkSouls3-Premonition.mp3'],
+      src: ['../music/DarkSouls3-Premonition.mp3'],
       //src: ['../music/Miles-Davis-SoWhat.mp3'],
       //src : ["../music/desole.mp3"],
       loop: true,
