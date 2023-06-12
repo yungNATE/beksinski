78410d85e10b50f9350a4c9de6b2f04c17ba3bfc
smanchajm <samuel.manchajm@gmail.com>
2023-06-12 16:12:20 +0200

    ctest cursor opacité

--- js/main2.js
+++ js/main2.js
@@ -2,7 +2,8 @@
 const mainJS = () => {
     // load homemade cursor library
     let script = document.createElement('script');
-    script.src = "/beksinski/js/libs/cursor/cursor_incontrolable.js";
+    //script.src = "/beksinski/js/libs/cursor/cursor_incontrolable.js";
+    script.src = "/beksinski/js/libs/cursor/cursorTrompette.js";
     document.head.appendChild(script);
 }
 window.addEventListener('load', mainJS);
