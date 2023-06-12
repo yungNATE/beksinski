9f09472b24d61cca4a1697a135ed55de5ecc0a2e
Nathan <72710011+yungNATE@users.noreply.github.com>
2023-06-12 09:57:13 +0200

    problm de casse dans le nom, RAS

--- JS/main.js
+++
@@ -1,8 +0,0 @@
-const mainJS = () => {
-
-    // load homemade cursor library
-    let script = document.createElement('script');
-    script.src = "/beksinski/js/libs/cursor/cursor.js";
-    document.head.appendChild(script);
-}
-window.addEventListener('load', mainJS);
