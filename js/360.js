7b23e3d32f95ab1a22790e96cd048edb8504248a
yungNATE <nathan.martinigol@gmail.com>
2023-06-13 00:11:49 +0200

    transition multicolore -> chaud wip

---
+++ js/360.js
@@ -0,0 +1,26 @@
+// Chargement des libs
+// Three.js 105
+var threeScript = document.createElement('script');
+threeScript.src = "js/libs/three/105/three.min.js";
+document.head.appendChild(threeScript);
+
+threeScript.onload = () => {
+
+    // Panolens.js
+    var panolensScript = document.createElement('script');
+    panolensScript.src = "js/libs/panolens/panolens.js";
+    document.head.appendChild(panolensScript);
+
+}
+
+
+
+const threeSixtyMain = () => {
+
+    // const panorama = new PANOLENS.ImageLittlePlanet( '../../beksinski/media/img/fieldTest.jpg' );
+    const panorama = new PANOLENS.ImagePanorama( '../../beksinski/media/img/fieldTest.jpg' );
+    const viewer = new PANOLENS.Viewer();
+    viewer.add( panorama );
+
+}
+addEventListener('load', threeSixtyMain);
--- js/choice.js
+++ js/choice.js
@@ -3,66 +3,83 @@ const virtualLink = {
     offsetX: 0,
     offsetY: 0,
     proximiteAvecLeCurseur: 0,
-    lien: '',
+    fragmentACharger: '',
   
     //* Méthodes
-    constructor: function(offsetX, offsetY, link) {
+    constructor: function(offsetX, offsetY, fragmentACharger) {
         this.offsetX = offsetX;
         this.offsetY = offsetY;
-        this.lien = link;
+        this.fragmentACharger = fragmentACharger;
     },
 
     calculerDistance: function() {
       const self = this; // Référence à l'objet "virtualLink" à l'intérieur de la fonction callback
   
-      document.addEventListener('mousemove', function(e) {
+      document.addEventListener('mousemove', e => updateDistance(e));
+      const updateDistance = (e) => {
         var y = e.clientY / (window.innerHeight * self.offsetY);
         var x = e.clientX / (window.innerWidth * self.offsetX);
         var pos1 = -(Math.pow(y - 0.5, 2) + Math.pow(x - 0.5, 2)) * 4 + 1;
         
         self.proximiteAvecLeCurseur = pos1; // Met à jour la valeur de proximiteAvecLeCurseur
-        console.log(self.proximiteAvecLeCurseur);
-      });
+      }
     },
   
     checkEnContinuDuClic: function() {
       const self = this; // Référence à l'objet "virtualLink" à l'intérieur de la fonction callback
   
-      document.addEventListener('click', function(e) {
+      document.addEventListener('click', e => virtualLinkClicked(e));
+      const virtualLinkClicked = (e) => {
         if(self.proximiteAvecLeCurseur < 0.8) return
-        
-        // fade out
-        /*let fadeDuration = 1; //en secondes
-        sound.fade(vol, 0, fadeDuration*1000);
 
-        var body = document.querySelector('body');
-        body.style.backgroundColor = "black";
+        switchToFragment(self.fragmentACharger)
+      }
 
-        body.querySelectorAll(':scope > *').forEach(element => {
-            element.style.transition = `opacity ${fadeDuration}s ease-out`;
-            element.style.opacity = 0;
-        });*/
-
-        // Changement de page (après le fade out)
-        // setTimeout(() => {
-            window.location.href = self.lien;
-        // }, fadeDuration*1000);
-      });
     }
   };
 const choiceJS = () => {
     
     // définition de la position du lien 1
     const virtualLink1 = Object.create(virtualLink);
-    virtualLink1.constructor(0.70, 0.75, document.querySelector("#hotLink").href);
+    virtualLink1.constructor(0.70, 0.75, "hot");
     virtualLink1.calculerDistance();
     virtualLink1.checkEnContinuDuClic();
 
     // définition de la position du lien 2
     const virtualLink2 = Object.create(virtualLink);
-    virtualLink2.constructor(1.2, 1.3, document.querySelector("#coldLink").href);
+    virtualLink2.constructor(1.2, 1.3, "cold");
     virtualLink2.calculerDistance();
     virtualLink2.checkEnContinuDuClic();
 
 }
 window.addEventListener('DOMContentLoaded', choiceJS);
+
+function switchToFragment(fragment) {
+
+  switch (fragment) {
+    case "hot":
+      // "unload" current script
+      // ! NOT WORKING
+      // TODO
+      document.removeEventListener('mousemove', updateDistance);
+      document.removeEventListener('click', virtualLinkClicked);
+
+
+      // load 360.js
+      const script = document.createElement('script');
+      script.src = 'js/360.js';
+      document.head.appendChild(script);
+      document.head.removeChild(document.querySelector("#choiceScript"));
+
+      
+      break;
+
+    case "cold":
+      
+      break;
+  
+    default:
+      break;
+  }
+
+}
