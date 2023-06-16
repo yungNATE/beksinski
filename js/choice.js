527defb74c7c91b6d253733e7aa1b7c6ad61f0c7
yungNATE <nathan.martinigol@gmail.com>
2023-06-16 14:51:16 +0200

    avancée choice + test img 360

--- js/choice.js
+++ js/choice.js
@@ -1,70 +1,55 @@
-// TODO : régler problème listeners + 360.js chargé n fois
 const virtualLink = {
     //* Attributs
     self: null,
     offsetX: 0,
     offsetY: 0,
     proximiteAvecLeCurseur: 0,
-    fragmentACharger: '',
+    linkToFragment: '',
   
     //* Méthodes
-    constructor: function(offsetX, offsetY, fragmentACharger) {
+    constructor: function(offsetX, offsetY, linkToFragment) {
         this.offsetX = offsetX;
         this.offsetY = offsetY;
-        this.fragmentACharger = fragmentACharger;
+        this.linkToFragment = linkToFragment;
         this.self = this;
+
+        this.checkForClickedListener();
+    },
+  
+    // Click functions
+    checkForClicked: function() {
+        if(this.proximiteAvecLeCurseur < 0.8) return
+            
+
+        // Switch to fragment
+        switch (this.linkToFragment) {
+            case "hot.html":
+                // navigate to hot fragment
+                break;
+
+            case "cold.html":
+                break;
+
+            default:
+                break;
+        }
+        
+        window.location.href = this.linkToFragment;
+    },
+    checkForClickedListener: function() {
+        document.addEventListener('click', this.checkForClicked.bind(this));
     },
 
     // Mousemove functions
-    calculerDistance: function(e){
-
+    updateDistance: function(e){
       var y = e.clientY / (window.innerHeight * this.offsetY);
       var x = e.clientX / (window.innerWidth * this.offsetX);
       var pos1 = -(Math.pow(y - 0.5, 2) + Math.pow(x - 0.5, 2)) * 4 + 1;
       
       this.proximiteAvecLeCurseur = pos1; // Met à jour la valeur de proximiteAvecLeCurseur
     },
-    updateDistance: function() {
-      document.addEventListener('mousemove', this.calculerDistance);
-    },
-    removeUpdateDistancelistener: function() {
-      document.removeEventListener('mousemove', this.calculerDistance);
-    },
-  
-    // Click functions
-    virtualLinkClicked: function(e) {
-      if(this.proximiteAvecLeCurseur < 0.8) return
-            
-
-      // Switch to fragment
-      // "unload" current script
-      this.removeUpdateDistancelistener();
-      this.removeCheckEnContinuDuCliclistener();
-      
-      switch (this.fragmentACharger) {
-        case "hot":
-          // load 360.js
-          const script = document.createElement('script');
-          script.src = 'js/360.js';
-          document.head.appendChild(script);
-          // document.head.removeChild(document.querySelector("#choiceScript"));
-
-          break;
-
-        case "cold":
-          
-          break;
-      
-        default:
-          break;
-  }
-    },
-    checkEnContinuDuClic: function() {
-      document.addEventListener('click', this.virtualLinkClicked);
-    },
-    removeCheckEnContinuDuCliclistener: function() {
-      console.log("removeCheckEnContinuDuCliclistener");
-      document.removeEventListener('click', this.virtualLinkClicked);
+    updateDistanceListener: function() {
+      document.addEventListener('mousemove', this.updateDistance.bind(this));
     },
 };
 const choiceJS = () => {
@@ -72,14 +57,14 @@ const choiceJS = () => {
     // définition de la position du lien 1
     const virtualLink1 = Object.create(virtualLink);
     virtualLink1.constructor(0.70, 0.75, "hot");
-    virtualLink1.updateDistance();
-    virtualLink1.checkEnContinuDuClic();
+    virtualLink1.updateDistanceListener();
+    virtualLink1.checkForClickedListener();
 
     // définition de la position du lien 2
     const virtualLink2 = Object.create(virtualLink);
     virtualLink2.constructor(1.2, 1.3, "cold");
-    virtualLink2.updateDistance();
-    virtualLink2.checkEnContinuDuClic();
+    virtualLink2.updateDistanceListener();
+    virtualLink2.checkForClickedListener();
 
 }
 window.addEventListener('DOMContentLoaded', choiceJS);
